ebe7b2e3e7bf0c83acd6aa880fca3dbbdb2f2a65
Franktian <tianyawen201209@hotmail.com>
2015-02-01 18:26:07 -0500

    Connect animation to the penguin arm

--- GLWidget.h
+++ GLWidget.h
@@ -221,8 +221,8 @@ public:
     static const int JOINT_MAX = 45;
     
     // Other ranges
-    static const int FRANK_MIN = -90;
-    static const int FRANK_MAX = 90;
+    static const int ARM_MIN = -90;
+    static const int ARM_MAX = 90;
 
     GLWidget(QWidget *parent=NULL);
 
